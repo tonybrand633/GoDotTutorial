32cc13375883cb54a5d87d2ec16eb00053069b77
tonybrand <tonybrandTB37@gmail.com>
2023-09-23 17:18:53 +0800

    这个引擎的学习到此为止，继续UIToolKit

--- Scripts/Player/Frog.gd
+++ Scripts/Player/Frog.gd
@@ -1,9 +1,28 @@
 extends CharacterBody2D
 
-
+var SPEED = 40
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var player
+var chase = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	velocity.y += gravity * delta
 	
+	if chase == true:
+		player = get_node("../../Player/Player")
+		var direction = (player.position - self.position).normalized()
+		if direction.x>0:			
+			get_node("AnimatedSprite2D").flip_h = true
+		else:
+			get_node("AnimatedSprite2D").flip_h = false
+			
+		velocity.x = direction.x*SPEED				
+	move_and_slide()
+	
+func _on_player_2d_body_entered(body):
+	if body.name == "Player":
+		chase = true
+	else:
+		chase = false
+		
